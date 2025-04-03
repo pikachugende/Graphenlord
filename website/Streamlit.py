7b1f7afd71d06f014dc106b01b8264346e26c603
Tadeas Reindl <REI240763@spengergasse.at>
2025-04-03 17:04:34 +0200

    Update Streamlit.py

--- website/Streamlit.py
+++ website/Streamlit.py
@@ -3,14 +3,8 @@ import numpy as np
 import random
 
 def generate_response():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    return response
+    responses = ["Ich hab nen IQ von 85 — ääh von 85 ————— doch 85 stimmt schon jetzt bin Ich selber bisschen verwird quesen grade —— wegen der 38. Ich hab nen IQ von 35 ——— ne von 85 und ääh ——— odda wars 58? ——— ich weiß es gar nimmah ——— ich habs jetzt grad garnimma im Kopf —— is ja auch egal.", "Wer bei der Deutschen Bahn hat immer im Zug nen festen Sitzplatz? A: Der Zugführer", "Mein IQ ist wahrscheinlich höher als du, du Arschloch, denn ich weiß zumindest, wie man sich benimmt, du dummes Stück Scheiße.", "[...] die Tatsache die Tatsache, dass ich jemanden auffordere zu mir zu kommen, rechtfertigt auch noch nicht die Tatsache, dass es wirklich jemand umsetzt.", "Egal wie viele Haider mit mir in einem Raum eingesperrt sind, ich bin immer noch der größte Lappen von allen!", "Ich hab noch nie jemanden die Schuld gegeben, wenn ich selbst Schuld daran war.", "Sorry, aber ich bin göttlich, was meine Selbstbeherrschung angeht.", "Frankfurt ist ja glaube ich in Hessen, ich kenne mich geologisch immer nicht aus", "Ich hab keine ahnung wohin ich zieh letzten endes, muss ich gucken dann wenn es soweit ist.", "Hoffentlich kommt irchendwann mal a riesicher Meteor, der auf alle Fußballspieler dieser Welt fällt.", "Mein Leben kann man ein bisschen mit dem Tod von Jesus vergleichen.", "Der Durchschnitt ist immer so ne Durchschnittszahl.", "2011 war... wurde ich 12.", "Ich hör dich ned, du Spaggne, ich hab Musik an!", "Ihr seid genauso Terroristen wie die Terroristen, die irgendwas in die Luft jagen, mit dem einzigen Unterschied, dass noch, NOCH keiner zu Schaden gekommen ist. Bis jetzt, ihr Terroristen-Kaschber, alder.", "When the eagle cries, das bedeutet: Wenn der Adler kreist.", "What's going you on with you face, have you Brainschaden?!", "ich bin ned der Drache ferdammde aggsd!", "Die ham bei mir etzadla a riesiche Bombe neigschmaßt, die Arschlöscher!", "Verpiss dich etzadla aus meinem Land, alder!", "Ja, ich nenne ihn Mahmut, weil er sich wie einer aufführt.", "Gott kommt zu mir und will von mir gesegnet werden und nicht andersrum. Ich bin das höchste Wesen, was es gibt.", "IHR WERDET MICH NIEMALS BESIEGEN! IHR HABT'S NED DRAUF! IHR SEID NICHTS AUSSER SCHEISSE!", "Ich bin nicht derjeniche, der hier die Scheiße baut.", "Es gibt nicht zu jung, sondern nur zu eng.", "Eine Umdrehung der Erde ist ein Tag. Eine Umdrehung der Sonne ist ein Jahr.", "Nachts ist es kälter als draußen.", "Alter was bist'n du für'n neunzehnhundert ähh neunzig also neunzehnhundert nuller alter, was bist du für neunzehner ja ähh was bist du für neunzehntes neunzehnte neunzehntes Jahrhundert alter?", "Ich fahr nicht gerne Motorrad, weil's cool ist, ich fahr Motorrad, weil ich das gerne fahr. Basta. Abgesehen davon fahr ich kein Motorrad.", "Der Großteil der Hader ist Arbeitslos oder Arbeitet auf dem Bau weil sie zu dumm sind aus ihrem Leben mehr zu machen."]
+    return random.choice(responses)
     
 st.set_page_config(page_title="Graphenlord v1.0", layout="wide")
 
