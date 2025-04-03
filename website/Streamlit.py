d5cddef00c1dd7122421cf8ff8f424d5f97d69b1
Tadeas Reindl <REI240763@spengergasse.at>
2025-04-03 16:30:01 +0200

    Update Streamlit.py

--- website/Streamlit.py
+++ website/Streamlit.py
@@ -111,8 +111,28 @@ with col2:
                             unsafe_allow_html=True)
                 
 
-prompt = st.chat_input("Experten kontaktieren")
-with st.chat_message("Graphenlord[EXPERTE]"):
-    st.write("Meddl")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+
+
+st.title("ChatGPT-like clone")
+
+
+
+
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
